f5a2175607ca51f0f038efb16541af56cdaabbd9
lehoangq@gmail.com <lehoangq@gmail.com@2e56ffda-155b-7872-b1f3-609f5c043f28>
2013-12-17 13:34:33 +0000

    + add build_so.sh script (forgot before)

---
+++ HQEngine/Android/test/build_so.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ../HQEngine
+source setEnv.sh
+$ndk_build_script -j 8
+
+cd ../HQAudio
+$ndk_build_script -j 8
+
+cd ../HQSceneManagement
+$ndk_build_script -j 8
+
+cd ../test
+$ndk_build_script -j 8
