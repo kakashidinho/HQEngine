ed5be28dd4bc7cad3fca83238dff4c0f5be7b429
Kakashidinho <lehoangq@gmail.com>
2014-03-23 09:32:57 +0800

    +added VS2013 Desktop solution

--- HQEngine/Source/HQAudio/xaudio2/xFunctionPointers.h
+++ HQEngine/Source/HQAudio/xaudio2/xFunctionPointers.h
@@ -10,8 +10,8 @@ COPYING.txt included with this distribution for more information.
 
 #pragma once
 
-#include <XAudio2.h>
-#include <X3DAudio.h>
+#include "XAudio2.h"
+#include "X3DAudio.h"
 
 typedef void (*fpX3DAudioInitialize_t) (UINT32 SpeakerChannelMask, FLOAT32 SpeedOfSound, __out X3DAUDIO_HANDLE Instance);
 typedef void (*fpX3DAudioCalculate_t) (__in const X3DAUDIO_HANDLE Instance, __in const X3DAUDIO_LISTENER* pListener, __in const X3DAUDIO_EMITTER* pEmitter, UINT32 Flags, __inout X3DAUDIO_DSP_SETTINGS* pDSPSettings);
--- HQEngine/Source/HQEngine/gen_effect_script_parser.hpp
+++ HQEngine/Source/HQEngine/gen_effect_script_parser.hpp
@@ -1,8 +1,10 @@
-/* A Bison parser, made by GNU Bison 2.5.  */
 
-/* Bison interface for Yacc-like parsers in C
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
    
-      Copyright (C) 1984, 1989-1990, 2000-2011 Free Software Foundation, Inc.
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
    
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -64,7 +66,7 @@
 typedef union YYSTYPE
 {
 
-/* Line 2068 of yacc.c  */
+/* Line 1676 of yacc.c  */
 #line 42 "effect_script_parser.y"
 
 	struct {
@@ -81,8 +83,8 @@ typedef union YYSTYPE
 
 
 
-/* Line 2068 of yacc.c  */
-#line 86 "gen_effect_script_parser.hpp"
+/* Line 1676 of yacc.c  */
+#line 88 "gen_effect_script_parser.hpp"
 } YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
