58dfae2c59134fdb4a0d2502fb304900b4cdac1c
Kakashidinho <lehoangq@gmail.com>
2014-05-20 21:20:32 +0800

    - use pixel location directly instead of passing it via vertex shader

--- samples/Data/multires_splatting/final-pass.hlsl
+++ samples/Data/multires_splatting/final-pass.hlsl
@@ -7,7 +7,6 @@ struct VIN {
 
 struct VOUT {
 	float4 pos : SV_POSITION;
-	float2 texcoords: TEXCOORDS;
 };
 
 VOUT VS(in VIN input)
@@ -16,20 +15,19 @@ VOUT VS(in VIN input)
 
 	output.pos.xy = input.pos;
 	output.pos.zw = float2(0.0, 1.0);
-	output.texcoords = scaleToTexcoord(input.pos);
 	return output;
 }
 
 /*--------pixel shader------------*/
 Texture2D<float4> gbuffer_direct_flux : TEXUNIT0;//direct illumination flux
-uniform SamplerState gbuffer_direct_flux_sampler : TEXUNIT0;
 Texture2D<float4> final_interpolated_illum : TEXUNIT1;//final interpolated indirect illumination
-uniform SamplerState final_interpolated_illum_sampler : TEXUNIT1;
 
 float4 PS(in VOUT input) : SV_Target0
 {
-	float4 color = gbuffer_direct_flux.Sample(gbuffer_direct_flux_sampler, input.texcoords);
-	color += final_interpolated_illum.Sample(final_interpolated_illum_sampler, input.texcoords);
+	float2 pixelPos = input.pos.xy - float2(0.5, 0.5);
+	int2 iPixelPos = int2(pixelPos);
+	float4 color = gbuffer_direct_flux.Load(uint3(iPixelPos, 0));
+	color += final_interpolated_illum.Load(uint3(iPixelPos, 0));
 
 	return color;
 }
